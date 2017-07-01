da8e1e274814b40dbd66500e9f553cb09afb3ff7
s.maslow <stasmaslov98@gmail.com>
2017-07-01 13:22:24 +0400

    First commit

---
+++ urlrewrite.php
@@ -0,0 +1,35 @@
+<?
+$arUrlRewrite = array(
+	array(
+		"CONDITION" => "#^/online/([\\.\\-0-9a-zA-Z]+)(/?)([^/]*)#",
+		"RULE" => "alias=\$1",
+		"ID" => "bitrix:im.router",
+		"PATH" => "/desktop_app/router.php",
+	),
+	array(
+		"CONDITION" => "#^/bitrix/services/ymarket/#",
+		"RULE" => "",
+		"ID" => "",
+		"PATH" => "/bitrix/services/ymarket/index.php",
+	),
+	array(
+		"CONDITION" => "#^/online/(/?)([^/]*)#",
+		"RULE" => "",
+		"ID" => "bitrix:im.router",
+		"PATH" => "/desktop_app/router.php",
+	),
+	array(
+		"CONDITION" => "#^/photo/#",
+		"RULE" => "",
+		"ID" => "bitrix:photogallery",
+		"PATH" => "/photo.php",
+	),
+	array(
+		"CONDITION" => "#^/#",
+		"RULE" => "",
+		"ID" => "bitrix:blog",
+		"PATH" => "/index.php",
+	),
+);
+
+?>
