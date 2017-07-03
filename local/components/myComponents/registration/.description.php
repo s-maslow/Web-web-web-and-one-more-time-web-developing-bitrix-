81efb8f76c9d718ecb11912992a636728145a32b
s.maslow <stasmaslov98@gmail.com>
2017-07-03 17:57:38 +0400

    Writen .description.php

--- local/components/myComponents/registration/.description.php
+++ local/components/myComponents/registration/.description.php
@@ -1,23 +1,11 @@
 <?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 $arComponentDescription = array(
    "NAME" => "myRegistration",
    "DESCRIPTION" => "This is my registration",
    "ICON" => "/images/icon.gif",
    "PATH" => array(
-      "ID" => "content",
-      "CHILD" => array(
-         "ID" => "catalog",
-         "NAME" => "Каталог товаров"
-      )
-   ),
-   "AREA_BUTTONS" => array(
-      array(
-         'URL' => "javascript:alert('Это кнопка!!!');",
-         'SRC' => '/images/button.jpg',
-         'TITLE' => "Это кнопка!"
-      ),
-   ),
-   "CACHE_PATH" => "Y",
-   "COMPLEX" => "Y"
+      "ID" => "registration",
+   )
 );
 ?>
