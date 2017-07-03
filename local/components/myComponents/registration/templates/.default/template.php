360974d5e5cf61d5607a03f956cbdf521cbda078
s.maslow <stasmaslov98@gmail.com>
2017-07-03 18:31:10 +0400

    some changes in form

--- local/components/myComponents/registration/templates/.default/template.php
+++ local/components/myComponents/registration/templates/.default/template.php
@@ -3,7 +3,7 @@
 Регистрация<br>
 <form method="post" id="reg">
   Имя:<input type="text" name="name"><br>
-  E-mail:<input type="text" name="email"><br>
+  E-mail:<input type="email" name="email"><br>
   Пароль:<input type="password" name="pass"><br>
   Повторите пароль:<input type="password" name="confPass"><br>
   <input type="button" name="do it" value="далее"><br>
