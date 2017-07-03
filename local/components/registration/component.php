729e3143693d1a9fbc6bd07bf68ede9e43a188f4
s.maslow <stasmaslov98@gmail.com>
2017-07-03 11:29:09 +0400

    The form and some basic function was created

---
+++ local/components/registration/component.php
@@ -0,0 +1,25 @@
+<?
+if (check_bitrix_sessid() &&
+$_SERVER['REQUEST_METHOD'] == "POST" && !empty($_REQUEST["name"]) &&
+!empty($_REQUEST["email"]) &&
+!empty($_REQUEST["pass"]))
+{
+  $user = new CUser;
+  $userInf = Array(
+    "LOGIN" => trim($_REQUEST['email']),
+    "EMAIL" => trim($_REQUEST['email']),
+    "NAME" => trim($_REQUEST['name']),
+    "PASSWORD" => trim($_REQUEST['pass']),
+    "CONFIRM_PASSWORD" => trim($_REQUEST['confPass'])
+
+
+  );
+  $id = $use->Add($userInf);
+  if(intval($id) > 0){
+    echo "Вы успешно зарегестрированы"ж
+  } else {
+    echo $user->LAST_ERROR;
+  }
+}
+$this->IncludeComponentTemplate();
+?>
---
+++ local/components/registration/templates/.default/template.php
@@ -0,0 +1,13 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+?>
+Регистрация<br>
+<form method="post" id="reg">
+  Имя:<input type="text" name="name"><br>
+  E-mail:<input type="text" name="email"><br>
+  Пароль:<input type="password" name="pass"><br>
+  Повторите пароль:<input type="password" name="confPass"><br>
+  <input type="button" name="do it" value="далее"><br>
+</form>
+<?
+
+?>
