2164c453e48e619e85f7e815d3139f94ce91574a
s.maslow <stasmaslov98@gmail.com>
2017-07-05 23:54:14 +0400

    День закончился, стоит всё закомитить

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
 ?>
-
+<h1>HELLO WORLD</h1>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
--- local/templates/myTemplate/header.php
+++ local/templates/myTemplate/header.php
@@ -3,8 +3,6 @@ IncludeTemplateLangFile(__FILE__);
 ?>
 <!DOCTYPE html>
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?$APPLICATION->ShowHead();?>
     <title><?$APPLICATION->ShowTitle()?></title>
     <link rel="stylesheet" href="<?=CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH.'/style.css');?>">
@@ -17,3 +15,4 @@ IncludeTemplateLangFile(__FILE__);
         <header>
           <h1>HEADER</h1>
         </header>
+			</div>
